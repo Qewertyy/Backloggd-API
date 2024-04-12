e6bc25966099ccffedb639a28de7ea78a44224f5
Qewertyy <kh786100@gmail.com>
2024-04-12 17:17:46 +0530

    minor change.

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -36,7 +36,8 @@ async function getUserInfo(
   const $ = cheerio.load(response.data);
   let userinfo: userInfo = {} as userInfo;
   userinfo.username = username;
-  const userBio = $("#bio-body").children("p").children("i").text();
+  const hasBio = $("#bio-body").hasClass("rmjs-1")
+  const userBio = hasBio ? $("#bio-body").text() : "Nothing here!"
   userinfo.bio = userBio;
   const favoriteGames: favoriteGames[] = [];
   const recentlyPlayed: recentlyPlayed[] = [];
