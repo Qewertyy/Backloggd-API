e9ef151c0821806ba8010310643a192be6a0fdfd
Qewertyy <kh786100@gmail.com>
2024-04-14 19:27:10 +0530

    things.

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,15 +1,17 @@
 //Copyright 2023 Qewertyy, MIT License
 
 import { Router, Request, Response } from "express";
+import {repository} from "../../package.json";
 const Route = Router();
 
 function home(req: Request, res: Response) {
   const response = {
     code:2,
     message:"Unofficial Backloggd API.",
+    source:repository.url
   };
   res.status(200).json(response);
-}
+};
 
 
 Route
